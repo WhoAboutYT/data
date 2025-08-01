397241fc21687842f6cb64a5687670ed205213c9
WhoAboutYT <whoaboutyt854@gmail.com>
2025-08-01 08:43:10 -0500

    Update Protected_9945776628526155.lua

--- Protected_9945776628526155.lua
+++ Protected_9945776628526155.lua
@@ -23,8 +23,15 @@ local PlayerViewedEventFn = function(playerInfo)
 	if _G.Invisibility or _G.AutoArrest then 
 		warn("Respawning because you're being viewed while invis or autoarresting by", playerInfo.PlayerName)
 
-		if _G.AutoArrest then _G.AutoArrest = false end;
-		if _G.SpeedModify then _G.SpeedModify = false end;
+
+        -- This still dosent affect the ui visuals, but works better with the UI Toggle On/Off
+
+        _G.Window.TabModule.Tabs[1].Elements[8].Callback(false)
+        _G.Window.TabModule.Tabs[2].Elements[1].Callback(true)
+
+        ---------------
+
+        if _G.SpeedModify then _G.SpeedModify = false end;
 
 		RespawnFunction()
 	else
